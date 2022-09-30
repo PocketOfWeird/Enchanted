485ed39f771a7b848cbfe7f3d6c3ab1547a912eb
PocketOfWeird <hartzlernathan@gmail.com>
2022-09-29 20:40:24 -0500

    switch to ogg files

--- modules/touch_map.py
+++ modules/touch_map.py
@@ -3,12 +3,12 @@ from typing import List
 
 def touch_map(index: int) -> List: 
     files = {
-        0 : ["audio/switch_homestar.mp3","audio/switch_strongbad.mp3"],
-        1 : ["audio/hs_heystupid.mp3","audio/hs_canubelieveit.mp3","audio/hs_itsdotcom.mp3","audio/hs_notlookgood.mp3", "audio/hs_email.mp3", "audio/hs_error.mp3"],
-        2 : ["audio/sb_dubdeuce.mp3","audio/sb_system.mp3","audio/sb_pizza.mp3","audio/sb_email.mp3"],
-        3 : ["audio/bubs_dontutalk.mp3","audio/bubs_ladylike.mp3"],
-        4 : ["audio/cz_eatthechort.mp3","audio/cz_jeorghb.mp3"],
-        5 : ["audio/ss_frustrating.mp3","audio/ss_sadflying.mp3"],
-        6 : ["audio/song_everybody.mp3","audio/song_limit.mp3"]
+        0 : ["audio/switch_homestar.ogg","audio/switch_strongbad.ogg"],
+        1 : ["audio/hs_heystupid.ogg","audio/hs_canubelieveit.ogg","audio/hs_itsdotcom.ogg","audio/hs_notlookgood.ogg", "audio/hs_email.ogg", "audio/hs_error.ogg"],
+        2 : ["audio/sb_dubdeuce.ogg","audio/sb_pizza.ogg","audio/sb_email.ogg"],
+        3 : ["audio/bubs_dontutalk.ogg","audio/bubs_ladylike.ogg"],
+        4 : ["audio/cz_eatthechort.ogg","audio/cz_jeorghb.ogg"],
+        5 : ["audio/ss_frustrating.ogg","audio/ss_sadflying.ogg"],
+        6 : ["audio/song_everybody.ogg","audio/song_limit.ogg"]
     }
     return files[index]
