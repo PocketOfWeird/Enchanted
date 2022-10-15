ab4700c47515ea26d5cb59eae2b6e88564f90e01
PocketOfWeird <hartzlernathan@gmail.com>
2022-10-15 11:29:56 -0500

    tyring to integrate syth

--- enchanted.py
+++ enchanted.py
@@ -1,7 +1,5 @@
-from pygame import mixer
 from modules.touch_sensor import start_touch_sensor_loop
 from modules.touch_manager import touch_manager
 
-mixer.init()
 
 start_touch_sensor_loop(touch_manager)
